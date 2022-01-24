8df564a4af7d3cbb03c5f7fa48feb57a046109cb
Ranyar <r4n74r@gmail.com>
2022-01-24 18:01:52 +0000

    Deploying to gh-pages from @ Ranyar/kuznetsov-smartdevice@06fe6fd6c4fdc930481922e0c95c781b056c62b3 🚀

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const accordions = document.querySelectorAll('.accordion');
+const headers = document.querySelectorAll('.accordion h2');
+
+console.log(headers);
+
+console.log('sefesfesfsfsf');
+
+accordions.classList.remove('accordion--nojs');
