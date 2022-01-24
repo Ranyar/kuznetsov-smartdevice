dfd22ee63f17fdc780d1bae974be24fe4668a309
Ranyar <r4n74r@gmail.com>
2022-01-24 17:56:28 +0500

    Завершение стилизации и адаптива футера

---
+++ source/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const accordions = document.querySelectorAll('.accordion');
+const headers = document.querySelectorAll('.accordion h2');
+
+console.log(headers);
+
+accordions.classList.remove('accordion--nojs');
