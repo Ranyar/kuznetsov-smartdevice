8fdec534b1a4eab56a2ceb6c3ac0f6d0d7ba732f
Ranyar <r4n74r@gmail.com>
2022-01-25 07:54:16 +0000

    Deploying to gh-pages from @ Ranyar/kuznetsov-smartdevice@2357b243864b91ea0ea2b21e045afa9cf061ba83 🚀

--- js/main.js
+++ js/main.js
@@ -13,20 +13,20 @@ for (let i=0; i < headers.length; i++) {
     let shown = document.querySelector('.accordion__content--show');
     let minused = document.querySelector('.accordion--minus');
 
-    if (shown === headers[i]) { //Если ОТКРЫТЫЙ элемент - тот, по которому кликнули
-      content[i].classList.remove('accordion__content--show'); // Убрать у кликнутого ОТКРЫТЫЙ
-      minused.classList.add('accordion--plus'); // Добавить элементу с минусом плюс
-      minused.classList.remove('accordion--minus'); // Убрать у элемента с минусом минус
-    } else if (minused) { // Иначе если есть элемент с минусом
-        minused.classList.remove('accordion--minus'); // Убрать у элемента с минусом минус
-        minused.classList.add('accordion--plus'); // Добавить элементу с минусом плюс
-      }
-      content[i].classList.add('accordion__content--show'); // Добавить кликнутому элементу ОТКРЫТЫЙ
-      accordions[i].classList.remove('accordion--plus'); // Убрать у кликнутого элемента плюс
-      accordions[i].classList.add('accordion--minus'); // Добавить кликнутому элементу минус
+    if (shown === headers[i]) {
+      content[i].classList.remove('accordion__content--show');
+      accordions[i].classList.add('accordion--plus');
+      accordions[i].classList.remove('accordion--minus');
+    }
 
-      if (shown) {
-        shown.classList.remove('accordion__content--show'); // Если есть элемент с классом ОТКРЫТЫЙ, убрать класс
-      }
+    content[i].classList.add('accordion__content--show');
+    accordions[i].classList.remove('accordion--plus');
+    accordions[i].classList.add('accordion--minus');
+
+    if (shown) {
+      shown.classList.remove('accordion__content--show');
+      minused.classList.add('accordion--plus');
+      minused.classList.remove('accordion--minus');
+    }
   });
 }
