ea8ae7e03c18381710d727dc54774cd8befa9e10
Ranyar <r4n74r@gmail.com>
2022-01-26 01:17:03 +0500

    Главный блок (десктоп)

--- build/js/main.js
+++ build/js/main.js
@@ -13,20 +13,20 @@ for (let i=0; i < headers.length; i++) {
     let shown = document.querySelector('.accordion__content--show');
     let minused = document.querySelector('.accordion--minus');
 
-    if (shown === headers[i]) { //Если ОТКРЫТЫЙ элемент - тот, по которому кликнули
-      content[i].classList.remove('accordion__content--show'); // Убрать у кликнутого ОТКРЫТЫЙ
-      accordions[i].classList.add('accordion--plus'); // Добавить кликнутому с минусом плюс
-      accordions[i].classList.remove('accordion--minus'); // Убрать у кликнутого элемента минус
+    if (shown === headers[i]) {
+      content[i].classList.remove('accordion__content--show');
+      accordions[i].classList.add('accordion--plus');
+      accordions[i].classList.remove('accordion--minus');
     }
-    // Если кликнули по другому элементу
-    content[i].classList.add('accordion__content--show'); // Добавить кликнутому элементу ОТКРЫТЫЙ
-    accordions[i].classList.remove('accordion--plus'); // Убрать у кликнутого элемента плюс
-    accordions[i].classList.add('accordion--minus'); // Добавить кликнутому элементу минус
 
-    if (shown) { //При этом, если есть другой элемент с классом ОТКРЫТЫЙ
-      shown.classList.remove('accordion__content--show'); // убрать класс ОТКРЫТЫЙ
-      minused.classList.add('accordion--plus'); // Добавить элементу с минусом плюс
-      minused.classList.remove('accordion--minus'); // Убрать у элемента с минусом минус
+    content[i].classList.add('accordion__content--show');
+    accordions[i].classList.remove('accordion--plus');
+    accordions[i].classList.add('accordion--minus');
+
+    if (shown) {
+      shown.classList.remove('accordion__content--show');
+      minused.classList.add('accordion--plus');
+      minused.classList.remove('accordion--minus');
     }
   });
 }
